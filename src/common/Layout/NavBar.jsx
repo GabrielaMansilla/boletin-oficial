db4094ba397f70d0c4b8180952c98dc3a9175fa3
PedroAraozC <pedroaraozc@gmail.com>
2024-01-23 16:42:00 -0300

    comienzos de navBar

---
+++ src/common/Layout/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+export const NavBar = () => {
+  return (
+    <>
+    <div className='navCont '>
+        <div className="buscador container">
+
+        </div>
+
+    </div>
+    </>
+  )
+}
