527a9f438f05f52e48c20c28727015b5d45636ba
GabrielaMansilla <gmansilla.utn.frt@gmail.com>
2024-01-25 17:21:25 -0300

    se agrego iconos de redes sociales

--- src/common/Layout/Footer.jsx
+++ src/common/Layout/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
           {/* <ul className="list-unstyled"> */}
           <div className="col-md-6 mb-md-0 mb-3 ">
             <div className='d-flex justify-content-center mt-md-0 mt-3'>
-              <h5 className="px-4">Seguinos en </h5>
+              <h5 className="px-2">Seguinos en </h5>
               <div className=''>
                 <a className='p-2' href="https://www.facebook.com/?locale=es_LA"><i className="bi bi-facebook"></i></a>
                 <a className='p-2' href="https://www.instagram.com/"><i className="bi bi-instagram"></i></a>
@@ -21,7 +21,7 @@ export const Footer = () => {
               </div>
             </div>
             <div className='pt-3 '>
-              <p className='text-center text-md-end my-2 me-0 me-md-5  ' >Municipalidad de Tucumán</p>
+              <p className='text-center text-md-end my-2 me-0 me-md-5  ' >Municipalidad de San Miguel de Tucumán</p>
               <p className='text-center text-md-end my-2 me-0  me-md-5 '>9 de Julio 598 Tucumán, República Argentina</p>
               <p className='text-center text-md-end my-2 me-0 me-md-5'>(0381)- 4516500</p>
             </div>
