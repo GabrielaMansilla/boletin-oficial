a39f3c7b6f85c0b8f27d700ca54ce56896bccfda
PedroAraozC <pedroaraozc@gmail.com>
2024-01-29 19:02:35 -0300

    form alta boletines

--- src/components/AltaBoletines/AltaBoletines.jsx
+++ src/components/AltaBoletines/AltaBoletines.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './AltaBoletines.css'
+import { Box, FormControl, Input, InputLabel, MenuItem, Select, TextField, TextareaAutosize } from '@mui/material'
+import { BUSCADOR_VALUES } from '../../helpers/constantes'
+import FileUp from '@mui/icons-material/FileUpload';
+import File from '@mui/icons-material/UploadFileRounded';
 
 const AltaBoletines = () => {
+
+
+  const [values, setValues] = useState(BUSCADOR_VALUES)
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const [selectedFileName, setSelectedFileName] = useState('Seleccione un Archivo');
+
+  const handleChangeFile = (event) => {
+    const fileName = event.target.files[0]?.name || '';
+    setSelectedFileName(fileName);
+  }
+
   return (
-    <div>AltaBoletines</div>
+    <Box
+      component="form"
+      // sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
+      noValidate
+      autoComplete="off"
+      className='contAltaBoletines container'
+    >
+      <Box className="formGroup">
+
+        <TextField
+          label="Nro de Boletín"
+          className='inputAltaBoletin'
+          type='number'
+          value={values.nroBoletin}
+          onChange={handleChange}
+          inputProps={{ min: "0", max: "99999" }}
+          name="nroBoletin"
+        />
+        {/* <FormControl sx={{ m: 1, minWidth: 80 }}
+          className='inputAltaBoletin'
+        >
+
+          <InputLabel id="demo-simple-select-autowidth-label">Tipo</InputLabel>
+          <Select
+            value={values.tipo}
+            onChange={handleChange}
+            autoWidth
+            label="Tipo"
+            name="tipo"
+            className='selectAltaBoletin'
+
+          >
+            <MenuItem value="">
+              <em>---- Seleccione el tipo ----</em>
+            </MenuItem>
+            <MenuItem value={22}>Ordenanza</MenuItem>
+            <MenuItem value={10}>Decreto</MenuItem>
+            <MenuItem value={21}>Resolución</MenuItem>
+            <MenuItem value={22}>Licitaciones Públicas</MenuItem>
+          </Select>
+        </FormControl> */}
+        
+      </Box>
+
+      <TextareaAutosize
+        minRows={10} className='textAreaBoletines' />
+
+      <Box >
+        <label className='fileNameDisplay flex-column'>{selectedFileName}
+          <Input
+            className="inputFileAltaBoletin"
+            type='file'
+            name="fileBoletin"
+            onChange={handleChangeFile}
+          />
+          {selectedFileName == 'Seleccione un Archivo' ? (
+
+            <FileUp />
+          ) : (
+            <File />
+          )
+          }
+        </label>
+
+      </Box>
+    </Box>
   )
 }
 
--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, FormControl, InputLabel, MenuItem, Select, Text
 import React, { useState } from 'react'
 import './Buscador.css'
 import { BUSCADOR_VALUES } from '../../helpers/constantes'
+import { useLocation } from 'react-router-dom'
 
 const Buscador = () => {
 
@@ -9,67 +10,67 @@ const Buscador = () => {
 
     const handleChange = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
-      };
-      
+    };
 
+    const location = useLocation();
 
     return (
         <div className='d-flex justify-content-center'>
-
-            <Box className="buscador ">
-                <h3 className='tituloBuscador'>BUSCAR BOLETINES ANTERIORES</h3>
-                <Box
-                    component="form"
-                    sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
-                    noValidate
-                    autoComplete="off"
-                    className='inputCont container'
-                >
-                    <TextField
-                        label="Nro de Boletín"
-                        variant="outlined"
-                        className='inputBuscador'
-                        type='number'
-                        value={values.nroBoletin}
-                        onChange={handleChange}
-                        inputProps={{ min: "0" }}
-                        name="nroBoletin"
-                    />
-
-                    <TextField
-                        label="Fecha"
-                        variant="outlined"
-                        name="fecha"
-                        type="date"
-                        className='inputBuscador'
-                        value={values.fecha}
-                        onChange={handleChange}
-                        InputLabelProps={{ shrink: true }}
-                    />
-                    <FormControl sx={{ m: 1, minWidth: 80 }} >
-
-                        <InputLabel id="demo-simple-select-autowidth-label">Tipo</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-autowidth-label"
-                            id="demo-simple-select-autowidth"
-                            value={values.tipo}
+            {location.pathname === "/" &&
+                <Box className="buscador ">
+                    <h3 className='tituloBuscador'>BUSCAR BOLETINES ANTERIORES</h3>
+                    <Box
+                        component="form"
+                        sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
+                        noValidate
+                        autoComplete="off"
+                        className='inputCont container'
+                    >
+                        <TextField
+                            label="Nro de Boletín"
+                            variant="outlined"
+                            className='inputBuscador'
+                            type='number'
+                            value={values.nroBoletin}
                             onChange={handleChange}
-                            autoWidth
-                            label="Tipo"
-                            name="tipo"
-                        >
-                            <MenuItem value="">
-                                <em>---- Seleccione el tipo ----</em>
-                            </MenuItem>
-                            <MenuItem value={10}>Decreto</MenuItem>
-                            <MenuItem value={21}>Resolución</MenuItem>
-                            <MenuItem value={22}>Ordenanza</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <Button variant="contained" className='btnBuscador' type='submit'>Buscar</Button>
-                    <Button variant="contained" className='btnBuscadorAvanzada' >Busqueda Avanzada</Button>
-                </Box>
-            </Box>
+                            inputProps={{ min: "0" }}
+                            name="nroBoletin"
+                        />
+
+                        <TextField
+                            label="Fecha"
+                            variant="outlined"
+                            name="fecha"
+                            type="date"
+                            className='inputBuscador'
+                            value={values.fecha}
+                            onChange={handleChange}
+                            InputLabelProps={{ shrink: true }}
+                        />
+                        <FormControl sx={{ m: 1, minWidth: 80 }} >
+
+                            <InputLabel id="demo-simple-select-autowidth-label">Tipo de norma</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-autowidth-label"
+                                id="demo-simple-select-autowidth"
+                                value={values.tipo}
+                                onChange={handleChange}
+                                autoWidth
+                                label="Tipo de norma"
+                                name="tipo"
+                            >
+                                <MenuItem value="">
+                                    <em>---- Seleccione norma ----</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Decreto</MenuItem>
+                                <MenuItem value={21}>Resolución</MenuItem>
+                                <MenuItem value={22}>Ordenanza</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button variant="contained" className='btnBuscador' type='submit'>Buscar</Button>
+                        <Button variant="contained" className='btnBuscadorAvanzada' >Busqueda Avanzada</Button>
+                    </Box>
+                </Box>}
         </div>
 
     )
