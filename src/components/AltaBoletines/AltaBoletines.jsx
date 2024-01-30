205441bcb8833603e870c388e94ae310d54dd266
PedroAraozC <pedroaraozc@gmail.com>
2024-01-30 19:00:18 -0300

    avance de alta boletin

--- src/components/AltaBoletines/AltaBoletines.jsx
+++ src/components/AltaBoletines/AltaBoletines.jsx
@@ -1,25 +1,59 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './AltaBoletines.css'
-import { Box, FormControl, Input, InputLabel, MenuItem, Select, TextField, TextareaAutosize } from '@mui/material'
-import { BUSCADOR_VALUES } from '../../helpers/constantes'
+import { Box, Button, FormControl, Input, InputLabel, MenuItem, Select, TextField, TextareaAutosize } from '@mui/material'
+import { ALTA_BOLETIN_VALUES } from '../../helpers/constantes'
 import FileUp from '@mui/icons-material/FileUpload';
 import File from '@mui/icons-material/UploadFileRounded';
 
 const AltaBoletines = () => {
 
+  const [values, setValues] = useState(ALTA_BOLETIN_VALUES)
+  const [selectedFileName, setSelectedFileName] = useState('Seleccione un Archivo');
 
-  const [values, setValues] = useState(BUSCADOR_VALUES)
+  const obternerLista = (inicio, fin) => {
+    const inicioNum = parseInt(inicio, 10)
+    const finNum = parseInt(fin, 10)
 
+    if (!isNaN(inicioNum) && !isNaN(finNum)) {
+      return Array.from({ length: finNum - inicioNum + 1 }, (_, index) => inicioNum + index)
+    } else if (!isNaN(inicioNum)) {
+      return [inicioNum]
+    } else {
+      return []
+    }
+  }
+
+  const obtenerDecretos = () => {
+    return obternerLista(values.nroDecretoInicial, values.nroDecretoFinal)
+  };
+
+  const obtenerResoluciones = () => {
+    return obternerLista(values.nroResolucionInicial, values.nroResolucionFinal)
+  };
+
+  const obtenerLicitaciones = () => {
+    return obternerLista(values.nroLicitacionInicial, values.nroLicitacionFinal)
+  };
+
+  const handleGuardarBoletin = () => {
+    const boletin = {
+      decretos: obtenerDecretos(),
+      resoluciones: obtenerResoluciones(),
+      licitaciones: obtenerLicitaciones()
+    };
+
+    // Aquí deberías manejar el guardado del boletín en tu backend o donde corresponda
+    console.log('Boletín a guardar:', boletin);
+  };
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const [selectedFileName, setSelectedFileName] = useState('Seleccione un Archivo');
-
-  const handleChangeFile = (event) => {
-    const fileName = event.target.files[0]?.name || '';
+  const handleChangeFile = (e) => {
+    const fileName = e.target.files[0]?.name || '';
     setSelectedFileName(fileName);
-  }
+  };
+
 
   return (
     <Box
@@ -29,65 +63,128 @@ const AltaBoletines = () => {
       autoComplete="off"
       className='contAltaBoletines container'
     >
-      <Box className="formGroup">
+      <div className='d-flex flex-row justify-content-around w-100'> 
+        <Box className="formGroup flex-col ">
 
-        <TextField
+          <h3 className='nroBoletin'>Nro de Boletin: 4465
+            {/* ´ {values.nroBoletin}´ */}
+          </h3>
+          {/* <TextField
           label="Nro de Boletín"
           className='inputAltaBoletin'
           type='number'
           value={values.nroBoletin}
           onChange={handleChange}
-          inputProps={{ min: "0", max: "99999" }}
+          inputProps={{ min: "10000", max: "99999" }}
           name="nroBoletin"
-        />
-        {/* <FormControl sx={{ m: 1, minWidth: 80 }}
-          className='inputAltaBoletin'
-        >
+        /> */}
 
-          <InputLabel id="demo-simple-select-autowidth-label">Tipo</InputLabel>
-          <Select
-            value={values.tipo}
-            onChange={handleChange}
-            autoWidth
-            label="Tipo"
-            name="tipo"
-            className='selectAltaBoletin'
+          <div className='contRango'>
 
-          >
-            <MenuItem value="">
-              <em>---- Seleccione el tipo ----</em>
-            </MenuItem>
-            <MenuItem value={22}>Ordenanza</MenuItem>
-            <MenuItem value={10}>Decreto</MenuItem>
-            <MenuItem value={21}>Resolución</MenuItem>
-            <MenuItem value={22}>Licitaciones Públicas</MenuItem>
-          </Select>
-        </FormControl> */}
-        
-      </Box>
+            <h5>Decreto</h5>
 
-      <TextareaAutosize
-        minRows={10} className='textAreaBoletines' />
+            <div >
+              <TextField
+                label="Nº de Decreto inicial"
+                className='inputAltaBoletin'
+                type='number'
+                value={values.nroDecretoInicial}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroDecretoInicial"
+              />
+              <TextField
+                label="Nº de Decreto Final"
+                className='inputAltaBoletin ms-3'
+                type='number'
+                value={values.nroDecretoFinal}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroDecretoFinal"
+              />
+            </div>
+          </div>
 
-      <Box >
-        <label className='fileNameDisplay flex-column'>{selectedFileName}
-          <Input
-            className="inputFileAltaBoletin"
-            type='file'
-            name="fileBoletin"
-            onChange={handleChangeFile}
-          />
-          {selectedFileName == 'Seleccione un Archivo' ? (
+          <div className='contRango'>
 
-            <FileUp />
-          ) : (
-            <File />
-          )
-          }
-        </label>
+            <h5>Resolución</h5>
 
-      </Box>
-    </Box>
+            <div>
+              <TextField
+                label="Nº de Resolución Inicial"
+                className='inputAltaBoletin'
+                type='number'
+                value={values.nroResolucionInicial}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroResolucionInicial"
+              />
+              <TextField
+                label="Nº de Resolución Final"  
+                className='inputAltaBoletin ms-3'
+                type='number'
+                value={values.nroResolucionFinal}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroResolucionFinal"
+              />
+
+            </div>
+          </div>
+
+          <div className='contRango'>
+
+            <h5>Licitación</h5>
+
+            <div >
+              <TextField
+                label="Nº de Licitación Inicial"
+                className='inputAltaBoletin'
+                type='number'
+                value={values.nroLicitacionInicial}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroLicitacionInicial"
+              />
+              <TextField
+                label="Nº de Licitación Final"
+                className='inputAltaBoletin ms-3'
+                type='number'
+                value={values.nroLicitacionFinal}
+                onChange={handleChange}
+                inputProps={({ min: "1000", max: "999999" }, { minLength: 4, maxLength: 6 })}
+                name="nroLicitacionFinal"
+              />
+            </div>
+          </div>
+        </Box>
+
+        {/* <TextareaAutosize
+        minRows={10} className='textAreaBoletines' /> */}
+
+        <Box className="d-flex flex-column col-4 W-100">
+          <label className='fileNameDisplay flex-column'>{selectedFileName}
+            <Input
+              className="inputFileAltaBoletin"
+              type='file'
+              name="fileBoletin"
+              onChange={handleChangeFile}
+              required
+            />
+            {selectedFileName == 'Seleccione un Archivo' ? (
+
+              <FileUp />
+            ) : (
+              <File />
+            )
+            }
+          </label>
+        </Box>
+      </div>
+          <Button type="button" variant="contained" onClick={handleGuardarBoletin}>
+            Guardar Boletín
+          </Button>
+    </Box >
   )
 }
 
--- src/helpers/constantes.js
+++ src/helpers/constantes.js
@@ -1,10 +1,19 @@
 export const LOGIN_VALUES = {
-    nombreUsuario: "",
-    password: "",
-  };
-
-  export const BUSCADOR_VALUES = {
-    fecha: '',
-    nroBoletin: '',
-    tipo: '',
+  nombreUsuario: "",
+  password: "",
 };
+
+export const BUSCADOR_VALUES = {
+  fecha: '',
+  nroBoletin: '',
+  tipo: '',
+};
+export const ALTA_BOLETIN_VALUES = {
+  nroBoletin: '',
+  nroDecretoInicial: '',
+  nroDecretoFinal: '',
+  nroResolucionInicial: '',
+  nroResolucionFinal: '',
+  nroLicitacionInicial: '',
+  nroLicitacionFinal: '',
+}
