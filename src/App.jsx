978f14f62ba3e1331c799e01fd6779af2d5807ff
MarianoMartinez10 <nanomartinezbm@gmail.com>
2024-03-08 17:35:53 -0300

    creacion de tabla

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import AltaBoletines from './components/AltaBoletines/AltaBoletines';
 import Login from './components/Login/Login';
-
+import Tabla from "./components/Tabla/Tabla"
 import Buscador from './components/Buscador/Buscador';
 
 
@@ -17,6 +17,7 @@ const App = () => {
           <Route path="/*" element={<Buscador />} />
           <Route path="/login" element={<Login />} />
           <Route path="/altaBoletines" element={<AltaBoletines />} />
+          <Route path='/tabla' element={<Tabla />} />
         </Routes>
       </Layout>
     </Router>
