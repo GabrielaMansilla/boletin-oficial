d49f7e05573976a4c231912ede241588dee9b14e
MarianoMartinez10 <nanomartinezbm@gmail.com>
2024-03-08 18:25:42 -0300

    boton edit en acciones

--- src/components/Tabla/Tabla.jsx
+++ src/components/Tabla/Tabla.jsx
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import axios from "axios";
+import axios from "../../config/axios";
 import useGet from "../../hook/useGet";
-import { MenuItem, Select } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { useEffect, useState } from "react";
+
+
 
 const Tabla = () => {
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [tiposNorma, loadingNorma, getTiposNoma] = useGet("/norma/listar", axios);
-  const [boletines, loading, getboletin] = useGet("/boletin/listar", axios);
+    const [tiposNorma, loadingNorma, getTiposNoma] = useGet("/norma/listar", axios);
+    const [editNorma, setEditNorma] = useState ("");
  
   const columns = [
     { id: "tipo_norma", label: "Norma", minWidth: 100, align: "center" },
@@ -24,17 +23,17 @@ const Tabla = () => {
     { id: "acciones", label: "Acciones", minWidth: 100, align: "center", },
     
   ];
-console.log(tiposNorma);
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+ 
+  const handleEdit = (Norma) => {
+    setEditNorma((prevNorma) => ({ ...prevNorma, ...Norma }));
+    
   };
-
-  return (<>
+  useEffect(() => {
+    console.log(editNorma);
+  }, [editNorma]);
+    
+  return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
@@ -53,15 +52,21 @@ console.log(tiposNorma);
           </TableHead>
           <TableBody>
            { console.log(tiposNorma)}
-            {loadingNorma ? (
-              
-              
+            {!loadingNorma ? (
+                          
               tiposNorma
               .map((Norma, index) => (
                 <TableRow key={index}>
                   {columns.map((column) => (
                     <TableCell key={column.id} align={column.align}>
-                      {Norma[column.id]}
+                      
+                     { column.id === "acciones" ? (  
+                        <EditIcon
+                         onClick={() => handleEdit(Norma)}
+                        className="iconEdit"
+                      />
+                    ):(Norma[column.id])}
+
                     </TableCell>
                   ))}
                   </TableRow>
@@ -72,36 +77,7 @@ console.log(tiposNorma);
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-    </Paper>   <Select
-    labelId="demo-simple-select-autowidth-label"
-    id="demo-simple-select-autowidth"
-   
-    
-    autoWidth
-    label="Tipo de Norma"
-    name="tipoBusquedaAvanzada"
-  >
-    <MenuItem value="">
-      <em>--Seleccione--</em>
-    </MenuItem>
-    {tiposNorma.map((tipo) => (
-      <MenuItem key={tipo.id_norma} value={tipo.tipo_norma}>
-        {tipo.tipo_norma}
-      </MenuItem>
-    ))}
-    {/* <MenuItem value={"Ordenanza"}>Ordenanza</MenuItem>
-    <MenuItem value={"Resolucion"}>Resolución</MenuItem> */}
-  </Select>
-        </>
- 
+        </Paper>   
   );
 }
 
