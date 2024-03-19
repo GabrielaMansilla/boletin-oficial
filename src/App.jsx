520d8fc692c0aeddbe6daf841af5259ded362766
GabrielaMansilla <gmansilla.utn.frt@gmail.com>
2024-03-19 14:46:28 -0300

    tabla editar normas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AltaBoletines from './components/AltaBoletines/AltaBoletines';
 import Login from './components/Login/Login';
 import Buscador from './components/Buscador/Buscador';
 import ListadoBoletines from './components/ListadoBoletines/ListadoBoletines';
-
+import ListarNormas from './components/ListarNormas/ListarNormas';
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/altaBoletines" element={<AltaBoletines />} />
           <Route path="/listadoBoletines" element={<ListadoBoletines />} />
-         
+          <Route path="/listarNormas" element={<ListarNormas />} />
         </Routes>
       </Layout>
     </Router>
